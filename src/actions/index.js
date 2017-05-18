1ca37fd0600d1f08f43ef29c252b884d6af61a33
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2017-05-18 22:25:54 +0530

    redux promise initial

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,6 +14,8 @@ export function fetchWeather(city){
 	const url = `${ROOT_URL}&q=${city},in`;
 	const request  = axios.get(url);
 
+	console.log('Request: ', request);
+
 	return {
 		type: FETCH_WEATHER,
 		payload: request
